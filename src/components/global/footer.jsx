647912a051ecc709ef7f1fb4508fcc93a3b292da
G51DHU <gs_sidhu@hotmail.co.uk>
2022-01-08 00:08:47 +0000

    Respective CSS moved to footer.jsx.

--- src/components/global/footer.jsx
+++ src/components/global/footer.jsx
@@ -1,9 +1,22 @@
 import logo from '@a/logo.svg'
 
+const style = {
+    "footer":{
+        backgroundColor: "#2D5277",
+        position: "static",
+        width: "100vw",
+        maxWidth: "100%",
+        bottom: "0",
+    },
+    "footer > img": {
+        width: "20vw",
+        height: "20vh",
+    }
+}
 
 export default function FooterComp(){
     return(
-    <div className="Footer">
-        <img src={logo} alt="Logo"/>
+    <div style={style["footer"]}>
+        <img style={style['footer > img']} src={logo} alt="Logo"/>
     </div>)
 }
