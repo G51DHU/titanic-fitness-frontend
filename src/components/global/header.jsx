88b414ecde401090dc11830594a6cb28f0d5b5a0
G51DHU <gs_sidhu@hotmail.co.uk>
2022-01-08 01:13:03 +0000

    Relevent css moved to header.jsx. Buttons fixed,

--- src/components/global/header.jsx
+++ src/components/global/header.jsx
@@ -1,13 +1,49 @@
 import logo from "@a/logo.svg";
-import "@s/header.css"
 import { Link } from "react-router-dom";
 
+const style = { 
+    "header": {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        borderStyle: "solid",
+        borderWidth: "1px",
+        borderLeft: "none",
+        borderRight: "none",
+    
+        height: "100px",
+    
+        paddingLeft:  "10%",
+        paddingRight: "10%",
+    },
+    "logo":{
+        height:"150%",
+        flex:"1",
+        paddingRight: "50%",
+    },
+    "header-button": {
+        borderRadius: "10pt",
+        minWidth: "100px",
+    
+        height: "40%",
+        width: "20%",
+        margin: "10px",
+        backgroundColor: "#7DCCAB",
+        color: "black",
+    
+        display: "flex",
+        justifyContent: "center",
+        alignItems:"center",
+        textDecoration: "none",
+    },
+}
 export default function Header(props){
     return(
-        <div className="header">
-            <img className="logo" src={logo} alt="logo"></img> 
-            <Link className="login-button" to="/Login">Login</Link>
-            <Link className="sign-button" value="sign up" to="/Register">Register</Link>
-        </div>
+        <header style={style["header"]}>
+            <img style={style["logo"]} src={logo} alt="logo"></img> 
+            <Link style={style["header-button"]} to="/Login">Login</Link>
+            <Link style={style["header-button"]} value="sign up" to="/Register">Register</Link>
+        </header>
     )
 }
