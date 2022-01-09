c4d6057c760469b89bfaa7705941ebb18f5e6720
G51DHU <gs_sidhu@hotmail.co.uk>
2022-01-09 01:09:28 +0000

    Changed element names.

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,13 +1,15 @@
+import "@s/register.css"
 import { useState,useEffect } from "react"
 import { useNavigate } from "react-router"
 
 
 
-export default function RegisterComp(){
+export default function RegisterPage(){
     const [Username, setUsername] = useState("")
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
     const [ConfirmPassword, setConfirmPassword] = useState("")
+
     const [PasswordVisible, setPasswordVisible] = useState(false)
     const [ReadyToSubmit, setReadyToSubmit] = useState(false)
     const [minUppercase, setminUppercase] = useState(false)
@@ -16,6 +18,7 @@ export default function RegisterComp(){
     const [minLowercase, setminLowercase] = useState(false)
     const [minChar, setminChar] = useState(false)
     const [passwordCondtions, setPasswordConditions] = useState(false)
+
     const navigate = useNavigate()
 
     function updateUsername(event){
@@ -103,6 +106,7 @@ export default function RegisterComp(){
 
     function ValidateFORM(event){
         event.preventDefault()
+        
         passwordValidation(Password)
         if(minUppercase===true && minLowercase===true && minChar===true && minDigit===true && minSpecialChar===true){
             setPasswordConditions(true)
@@ -115,53 +119,58 @@ export default function RegisterComp(){
         else{
         }
     }
-    return(
-        <div className="Register-form-container">
-            <h1>Register</h1>
-            <form onSubmit={ValidateFORM}>
-                <div className="Username-container">
-                    <label>Username</label>
-                    <input className="input-username" type="text" onChange={updateUsername} />
-                </div>
-                <div className="Email-container">
-                    <label>Email</label>
-                    <input className="input-email" type="email" onChange={updateEmail} />
-                </div>
-                <div className="password-container">
-                    <div>
-                        <label>Password</label>
-                        <input 
-                        className="input-password" 
-                        type={PasswordVisible? "text":"password"}
-                        onChange={updatePassword} />
-                    </div>
-                    <div>
-                        <label>Confirm Password</label>
-                        <input 
-                        className="input-password" 
-                        type={PasswordVisible? "text":"password"}
-                        onChange={updateConfirmPassword} />
-                    </div>
-                </div>
-                
-                <div className="Password-Conditions">
-                    <label className={minUppercase? "password-req-met":"password-req-not"}>password requires at least 1 uppercase character</label>
-                    <label className={minLowercase? "password-req-met":"password-req-not"}>password requires at least 1 lowercase character</label>
-                    <label className={minSpecialChar? "password-req-met":"password-req-not"}>password requires at least 1 special character</label>
-                    <label className={minDigit? "password-req-met":"password-req-not"}>password requires at least 1 digit</label>
-                    <label className={minChar? "password-req-met":"password-req-not"}>password requires at least 8 characters</label>
-                </div>
 
-                <div className="checkbox-container">
-                    <input onChange={showPassword} type="checkbox" />
-                    <label>Show Password</label>
-                </div>
-                <input 
-                className={!ReadyToSubmit?"Disabled-Submitbtn"  : "Submitbtn" }
-                type="submit" 
-                value="Register"
-                disabled={!ReadyToSubmit}></input>
-            </form>
+
+    return(
+        <div className="register-form">
+                <form onSubmit={ValidateFORM}>
+                    <h1>Register</h1>
+                    <label>
+                        Username
+                        <input className="input-username" type="text" onChange={updateUsername} />
+                    </label>
+                    <label>
+                        Email
+                        <input className="input-email" type="email" onChange={updateEmail} />    
+                    </label>
+
+                    <section className="password-container">
+                        <label>
+                            Password
+                            <input 
+                                type={PasswordVisible? "text":"password"}
+                                onChange={updatePassword}> 
+                            </input>
+                        </label>
+                        <label>
+                            Confirm Password
+                            <input 
+                                type={PasswordVisible? "text":"password"}
+                                onChange={updateConfirmPassword}>
+                            </input>
+                        </label>
+                    </section>
+                    
+                    <div className="Password-Conditions">
+                        <label className={minUppercase? "password-req-met":"password-req-not"}>password requires at least 1 uppercase character</label>
+                        <label className={minLowercase? "password-req-met":"password-req-not"}>password requires at least 1 lowercase character</label>
+                        <label className={minSpecialChar? "password-req-met":"password-req-not"}>password requires at least 1 special character</label>
+                        <label className={minDigit? "password-req-met":"password-req-not"}>password requires at least 1 digit</label>
+                        <label className={minChar? "password-req-met":"password-req-not"}>password requires at least 8 characters</label>
+                    </div>
+
+                    <label>
+                        Show Password
+                        <input onChange={showPassword} type="checkbox" />
+                    </label>
+
+                    <input 
+                        className={!ReadyToSubmit?"submitbtn-disabled"  : "submitbtn-enabled" }
+                        type="submit" 
+                        value="Register"
+                        disabled={!ReadyToSubmit}>
+                    </input>
+                </form>
         </div>
     )
 }
