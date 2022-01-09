2140c50c57e2ffaa2053543b9be400b45001ebc6
G51DHU <gs_sidhu@hotmail.co.uk>
2022-01-09 19:00:41 +0000

    Imported css. CSS names changed.

--- src/components/global/navbar.jsx
+++ src/components/global/navbar.jsx
@@ -1,3 +1,4 @@
+import "@s/navbar.css"
 import { Link } from "react-router-dom"
 import homeButtonImg from '@a/home.svg'
 
@@ -8,19 +9,19 @@ export default function NavbarComp(){
 
 
     return(
-        <div className="Navbar">
+        <div className="navbar">
             <div className="Link-Container">
-                <Link className="PlansBtn" to="/User/Plans">Workout</Link>
+                <Link className="PlansBtn" to="/user/my-plans">Plans</Link>
                 <div className="Dropdown">
-                    <Link to="/User/Workout-Plans">My Workouts</Link>
-                    <Link to="/User/Diet-Plans">Diet Plans</Link>
+                    <Link to="/user/my-workouts">My Workouts</Link>
+                    <Link to="/user/my-meal-plans">Diet Plans</Link>
                 </div>
-                <Link to="/User/Statistics">Statistics</Link>
+                <Link to="/user/my-statistics">Statistics</Link>
             </div>
-                <Link to="/User"><img src={homeButtonImg} alt="Home" className="Home-Button" /></Link>
+                <Link to="/user"><img src={homeButtonImg} alt="Home" className="Home-Button" /></Link>
             <div className="Link-Container">
-                <Link to="/User/Goals">Goals</Link>
-                <Link to="/User/Blogs">My Blog</Link>
+                <Link to="/user/my-goals">Goals</Link>
+                <Link to="/user/my-blogs">My Blog</Link>
             </div>
         </div>
     )
