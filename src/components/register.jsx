3465e6d776e4c6f07071791bab6963f786c27e52
G51DHU <gs_sidhu@hotmail.co.uk>
2022-01-09 18:59:01 +0000

    Removed extra stuff. Bugs fixed.  CSS names changed.

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,6 +1,7 @@
 import "@s/register.css"
 import { useState,useEffect } from "react"
-import { useNavigate } from "react-router"
+import { Link, useNavigate  } from "react-router-dom"
+import logo from '@a/logo-cropped.svg'
 
 
 
@@ -17,82 +18,47 @@ export default function RegisterPage(){
     const [minSpecialChar, setminSpecialChar] = useState(false)
     const [minLowercase, setminLowercase] = useState(false)
     const [minChar, setminChar] = useState(false)
-    const [passwordCondtions, setPasswordConditions] = useState(false)
-
+    
     const navigate = useNavigate()
 
-    function updateUsername(event){
-        setUsername(event.target.value)
-    }
-
-    function updateEmail(event){
-        setEmail(event.target.value)
-    }
-
-    function updatePassword(event){
-        setPassword(event.target.value)
-    }
-
-    function updateConfirmPassword(event){
-        setConfirmPassword(event.target.value)
-    }
-
-    function showPassword(){
-        setPasswordVisible(!PasswordVisible)
-    }
-    
-
-
-    function passwordValidation(password,event){
-        
+    useEffect(() => {
         const minUPPERCASE = /[A-Z]+/
         const minDIGIT = /\d+/
         const minSPECIALCHARACTER = /\W+/
         const minLOWERCASE = /[a-z]+/
         const minCHARACTERS = /.{8,}/
 
-
-        let errors=[]
-
-        if(!password.match(minUPPERCASE)){
-            errors.push("password requires at least 1 uppercase character")
+        if(!Password.match(minUPPERCASE)){
             setminUppercase(false)
         }
         else{
             setminUppercase(true)
         }
-        if(!password.match(minLOWERCASE)){
-            errors.push("password requires at least 1 lowercase character")
+        if(!Password.match(minLOWERCASE)){
             setminLowercase(false)
         }
         else{
             setminLowercase(true)
         }
-        if(!password.match(minDIGIT)){
-            errors.push("password requires at least 1 number (0-9)")
+        if(!Password.match(minDIGIT)){
             setminDigit(false)
         }
         else{
             setminDigit(true)
         }
-        if(!password.match(minSPECIALCHARACTER)){
-            errors.push("password requires at least 1 special character (!,?,%,etc)")
+        if(!Password.match(minSPECIALCHARACTER)){
             setminSpecialChar(false)
         }
         else{
             setminSpecialChar(true)
         }
-        if(!password.match(minCHARACTERS)){
-            errors.push("password requires a minimum of 8 characters")
+        if(!Password.match(minCHARACTERS)){
             setminChar(false)
         }
         else{
             setminChar(true)
         }
-        console.log(errors)
-
-    }
-    
+    },[Password])
 
 
     useEffect(()=>{
@@ -102,75 +68,67 @@ export default function RegisterPage(){
         else{
             setReadyToSubmit(true)
         }
-    })
+    }, [Username, Email, Password, ConfirmPassword])
 
-    function ValidateFORM(event){
+    function SumbitRegistration(event){
         event.preventDefault()
-        
-        passwordValidation(Password)
         if(minUppercase===true && minLowercase===true && minChar===true && minDigit===true && minSpecialChar===true){
-            setPasswordConditions(true)
-            if (Password===ConfirmPassword && passwordCondtions===true){
-                navigate("/User")
+            if (Password===ConfirmPassword){
+                navigate("/user")
             }
-            else{ 
-            }
-        }
-        else{
         }
     }
 
 
     return(
         <div className="register-form">
-                <form onSubmit={ValidateFORM}>
-                    <h1>Register</h1>
+            <form onSubmit={SumbitRegistration}>
+                <h1>Register</h1>
+                <label className="label-username">
+                    Username
+                    <input className="input-username" type="text" onChange={e => setUsername(e.target.value)} />
+                </label>
+                <label className="label-email">
+                    Email
+                    <input className="input-email" type="email" onChange={e => setEmail(e.target.value)} />    
+                </label>
+
+                <section className="password-container">
                     <label>
-                        Username
-                        <input className="input-username" type="text" onChange={updateUsername} />
+                        Password
+                        <input 
+                            type={PasswordVisible? "text":"password"}
+                            onChange={e => setPassword(e.target.value)}> 
+                        </input>
                     </label>
                     <label>
-                        Email
-                        <input className="input-email" type="email" onChange={updateEmail} />    
+                        Confirm Password
+                        <input 
+                            type={PasswordVisible? "text":"password"}
+                            onChange={e => setConfirmPassword(e.target.value)}>
+                        </input>
                     </label>
+                </section>
+                <label className="label-show-password">
+                    Show Password
+                    <input onChange={e => setPasswordVisible(!PasswordVisible)} type="checkbox" />
+                </label>
 
-                    <section className="password-container">
-                        <label>
-                            Password
-                            <input 
-                                type={PasswordVisible? "text":"password"}
-                                onChange={updatePassword}> 
-                            </input>
-                        </label>
-                        <label>
-                            Confirm Password
-                            <input 
-                                type={PasswordVisible? "text":"password"}
-                                onChange={updateConfirmPassword}>
-                            </input>
-                        </label>
-                    </section>
-                    
-                    <div className="Password-Conditions">
-                        <label className={minUppercase? "password-req-met":"password-req-not"}>password requires at least 1 uppercase character</label>
-                        <label className={minLowercase? "password-req-met":"password-req-not"}>password requires at least 1 lowercase character</label>
-                        <label className={minSpecialChar? "password-req-met":"password-req-not"}>password requires at least 1 special character</label>
-                        <label className={minDigit? "password-req-met":"password-req-not"}>password requires at least 1 digit</label>
-                        <label className={minChar? "password-req-met":"password-req-not"}>password requires at least 8 characters</label>
-                    </div>
-
-                    <label>
-                        Show Password
-                        <input onChange={showPassword} type="checkbox" />
-                    </label>
-
-                    <input 
-                        className={!ReadyToSubmit?"submitbtn-disabled"  : "submitbtn-enabled" }
-                        type="submit" 
-                        value="Register"
-                        disabled={!ReadyToSubmit}>
-                    </input>
-                </form>
+                <input 
+                    className={!ReadyToSubmit?"submitbtn submitbtn-disabled"  : "submitbtn submitbtn-enabled" }
+                    type="submit" 
+                    value="Register"
+                    disabled={!ReadyToSubmit}>
+                </input>
+            </form>
+            <div className="Password-Conditions">
+                <label className={minUppercase? "password-req-met":"password-req-not"}>password requires at least 1 uppercase character</label>
+                <label className={minLowercase? "password-req-met":"password-req-not"}>password requires at least 1 lowercase character</label>
+                <label className={minSpecialChar? "password-req-met":"password-req-not"}>password requires at least 1 special character</label>
+                <label className={minDigit? "password-req-met":"password-req-not"}>password requires at least 1 digit</label>
+                <label className={minChar? "password-req-met":"password-req-not"}>password requires at least 8 characters</label>
+            </div>
+            <Link className="link-wrap-titanic-fitness-logo" to="/"><img src={logo} width="409" height="222" alt="Titanic Fitness logo"/></Link>
         </div>
     )
 }
